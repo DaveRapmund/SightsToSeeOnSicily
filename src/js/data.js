6858ba5f8006b7526bed55dd47234595af769cb1
Dave <daverapmund@gmail.com>
2019-07-25 01:00:25 +0200

    Added all texts

--- src/js/data.js
+++ src/js/data.js
@@ -1,75 +1,75 @@
 const data = {
   "cefalu": {
-    "heading": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    "carousel": `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                 nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                 reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                 pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                 culpa qui officia deserunt mollit anim id est laborum.`,
+    "heading": "A gorgeous town at the foot of La Rocca with an amazing atmosphere",
+    "carousel": `Cefalù is a small fishermen's town, based right at the bottom of La Rocca, a massive rock that seems
+    to sliding down into the sea. La Rocca is a nice challenge for climbers and once you're on top, you'll be treated
+    to a wonderful view over the beach, the town with its cathedral and you'll find the Temple of Diana there. If you're
+    just looking for a place to relax, Cefalù's got you covered. Do try out the wonderful Cannoli and icecream they sell there!`,
     "sightsTitle": [
       "Duomo di Cefalù", "Cefalù Beach"
     ],
     "sightsText": [
-      "Cefalu Sight Text 1",
-      "Cefalu Sight Text 2"
+      `The Duomo di Cefalù, or the cathedral of Cefalù is a Roman Catholic basilica. It's one of nine structures included in the
+      UNESCO World Heritage Site. The cathedral was erected in 1131 in the Norman architectural style. It dominates the skyline of
+      the surrounding medieval town of Cefalù. With it's beautiful artwork inside, it's definitely something you want to check out
+      if you've made the smart decision to visit Cefalù.`,
+      `The Cefalù beach is a sight to behold for sure. It's long, sandy and the sea is perfect for bathing. The beach runs between
+      the majority of hotels that are in the vicinity straight to the medieval town of Cefalù. Once you reach Cefalù, you're free to
+      continue along the beach and admire the boats of the fishermen that are laying around.`
     ]
   },
 
   "sicily": {
-    "heading": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    "carousel": `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                 nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                 reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                 pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                 culpa qui officia deserunt mollit anim id est laborum.`,
+    "heading": "Welcome to Sicily, the largest Island in the Mediterranean Sea with more culture than you could ever imagine!",
+    "carousel": `Sicily is one of the 20 regions of Italy. It's commonly referred to as Trinacria due to its triangular shape.
+                The beautiful island is home to plenty of incredible art, music, literature, cuisine and architecture. The summers are hot and dry,
+                while the winters are mild and wet. This typical Mediterranean climate makes Sicily a great destination for your next holiday.
+                There's so much to see and with its unique culture, there's always something new to learn.`,
     "sightsTitle": [
       "Scala dei Turchi", "Temple of Segesta"
     ],
     "sightsText": [
-      `I Love Moesje To The Moon En BEK Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-       sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris`,
-      "Sicily Sight Text 2"
+      `Scala dei Turchi (or Turkish Steps) is a beautiful rock-formation just on the coast of Realmonte. The rock consists entirely of Marna, a type of limestone.
+      The wind and weather conditions have turned this rock into something that resembles a staircase.`,
+      `The Temple of Segesta is a well preserved Doric temple, thought to have been build around 420BC by an architect from Athens.
+      It sits on top of a hill overlooking the Gulf of Castellammare providing a great view that complements the beautiful temple itself.`
     ]
   },
 
   "palermo": {
-    "heading": "Jip en Janneke Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    "carousel": `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                 nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                 reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                 pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                 culpa qui officia deserunt mollit anim id est laborum.`,
+    "heading": "The capital of Sicily where there's a lot to see and a lot to do",
+    "carousel": `Palermo, found in the north-western part of Sicily, is a wonderful city with influences from both the Middle East and the Mediterranean.
+    It's a lively city with all kinds of beautiful squares, architecture, markets and culinary traditions. It differs a lot from the small, romantic towns and
+    villages that Sicily has to offer. There's a lot of tourism and there's plenty to do. It has an airport and a significant underground economy. The city is
+    undergoing careful redevelopment, so you best book a trip if you want to see all the beautiful old buildings that Palermo has to offer.`,
     "sightsTitle": [
       "Fontana Pretoria", "Cappella Palatina"
     ],
     "sightsText": [
-      "Palermo Sight Text 1",
-      "Palermo Sight Text 2"
+      `This monumental fountain is located in the historic center of Palermo and represents the most important landmark of Piazza Pretoria. It was built in
+      Florence in 1554 and transferred to Palermo in 1574. The fountain itself represents the Twelve Olympians, other mythological figures, animals and the
+      rivers of Palermo.`,
+      `La Cappella Palatina, or the Palatine Chapel is the royal chapel of the Norman kings of Sicily. It was built upon an older chapel, which now serves
+      as the crypt. It was constructed in 1080 and took eight years to build. The chapel has a lot of beautiful mosaics, some featuring Latin inscriptions.`
     ]
   },
 
   "taormina": {
-    "heading": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    "carousel": `I Love Moesje To The Moon En BEK Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                 nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                 reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                 pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                 culpa qui officia deserunt mollit anim id est laborum.`,
+    "heading": "A lovely and busy little town consisting of a high- and a low part",
+    "carousel": `Taormina is considered one of the best destinations on Sicily. The town consists of two parts, one being high up the mountains where you will
+    find plenty of restaurants, great little shops and a lot of history. The lower part of Taormina has a lot of beaches nearby. Taormina sits close to the
+    Etna volcano, which you'll be able to see from almost anywhere in Taormina. You'll have to take the cable car to get from the top of Taormina to the bottom,
+    since the entire city is a car-free zone. It's placements makes sure that there are plenty of beautiful sights around.`,
     "sightsTitle": [
       "Teatro Greco", "Piazza IX Aprile"
     ],
     "sightsText": [
-      "Taormina Sight Text 1",
-      "Taormina Sight Text 2"
+      `Il Teatro Greco, or the Greek Theatre is an old theatre that was built in the third century BC. It's buillt for the most part of brick and is therefore
+       probably of Roman date. It has a diameter of 120 meter (or 390 feet). It's the second largest of it's kind on Sicily and is used frequently nowadays for
+       operatic and theatrical performances and concerts.`,
+      `Piazza IX Aprile is a square in the center of Taormina known for it's breathtaking view of the azure Ionian Sea and of Mount Etna. It's filled to the brim
+      with cafés and restaurants. On most days, there are people perfoming around the square and there's a wonderful cathedral that's open to visitors. If you're
+      lucky, you can even witness a wedding there, as they are held on Piazza IX Aprile frequently.`
     ]
   }
 }
