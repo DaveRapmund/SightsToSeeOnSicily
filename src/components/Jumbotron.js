b45cd4c612080f551ac9b282e0c94a71f65fcfed
Dave <daverapmund@gmail.com>
2019-08-14 15:17:44 +0200

    added transitions with react-transition-group

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 class Jumbotron extends Component{
   componentDidMount(){
@@ -10,18 +11,24 @@ class Jumbotron extends Component{
 
   render(){
     return(
-      <div className="row jumbotron-row">
-        <div className="col-12 jumbotron">
-          <h2>
-            <span className="part-1">SIGHTS </span>
-            <span className="part-2">TO </span>
-            <span className="part-3">SEE </span>
-            <span className="part-4">ON </span>
-            <span className="part-5">SICILY </span>
-          </h2>
-          <div className="fade-out"></div>
-        </div>
-      </div>
+      <TransitionGroup>
+        <CSSTransition in={true} appear={true} timeout={500} classNames="jt">
+          <div className="row jumbotron-row">
+            <div className="col-12 jumbotron">
+              <CSSTransition in={true} appear={true} timeout={1000} classNames="it">
+                <h2>
+                  <span className="part-1">SIGHTS </span>
+                  <span className="part-2">TO </span>
+                  <span className="part-3">SEE </span>
+                  <span className="part-4">ON </span>
+                  <span className="part-5">SICILY </span>
+                </h2>
+              </CSSTransition>
+              <div className="fade-out"></div>
+            </div>
+          </div>
+        </CSSTransition>
+      </TransitionGroup>
     )
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
 
 function Navbar(){
   return(
-    <div className="navbar-row row">
-      <nav className="navbar navbar-dark navbar-expand-sm col-12">
-        <button className="navbar-toggler m-auto" type="button" data-toggle="collapse" data-target="#toggleMenu">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      <div className="navbar-row row">
+        <nav className="navbar navbar-dark navbar-expand-sm col-12">
+          <button className="navbar-toggler m-auto" type="button" data-toggle="collapse" data-target="#toggleMenu">
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-        <div className="collapse navbar-collapse" id="toggleMenu">
-          <div className="navbar-nav text-center mt-2">
-            <Link className="nav-item nav-link mr-0 mr-sm-5 text-white" to="/">Home</Link>
-            <Link className="nav-item nav-link mr-0 mr-sm-5 text-white" to="/cefalu">Cefalù</Link>
-            <Link className="nav-item nav-link mr-0 mr-sm-5 text-white" to="/taormina">Taormina</Link>
-            <Link className="nav-item nav-link mr-0 mr-sm-5 text-white" to="/palermo">Palermo</Link>
+          <div className="collapse navbar-collapse" id="toggleMenu">
+            <div className="navbar-nav text-center mt-2">
+                <CSSTransition in={true} appear={true} timeout={300} classNames="lt1">
+                  <Link className="nav-item nav-link mr-0 mr-sm-5 text-white" to="/">Home</Link>
+                </CSSTransition>
+                <CSSTransition in={true} appear={true} timeout={400} classNames="lt2">
+                  <Link className="nav-item nav-link mr-0 mr-sm-5 text-white" to="/cefalu">Cefalù</Link>
+                </CSSTransition>
+                <CSSTransition in={true} appear={true} timeout={500} classNames="lt3">
+                  <Link className="nav-item nav-link mr-0 mr-sm-5 text-white" to="/taormina">Taormina</Link>
+                </CSSTransition>
+                <CSSTransition in={true} appear={true} timeout={600} classNames="lt4">
+                  <Link className="nav-item nav-link mr-0 mr-sm-5 text-white" to="/palermo">Palermo</Link>
+                </CSSTransition>
+            </div>
           </div>
-        </div>
-      </nav>
-    </div>
+        </nav>
+      </div>
   )
 }
 
